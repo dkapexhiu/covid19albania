59f7d59a3a90cb0f935500dadd3fe87914bfbaa3
Daniel <daniel.kapexhiu@gmail.com>
2020-05-13 15:30:01 +0200

    Update Navbar.js

--- src/components/table1/Navbar.js
+++ src/components/table1/Navbar.js
@@ -5,9 +5,9 @@ function Navbar() {
     <div>
       <h1>Covid-19 ne Shqiperi</h1>
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <p>Konfirmuar: 876&nbsp;</p>
+        <p>Konfirmuar: 880&nbsp;</p>
         <p>Vdekur: 31&nbsp;</p>
-        <p>Sheruar: 682</p>
+        <p>Sheruar: 688</p>
       </div>
     </div>
   );
